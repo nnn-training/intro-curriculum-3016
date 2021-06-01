b2a80152af801991edb0104fe1f32e2a3e2568dc
あなたの名前 <isaco130@gmail.com>
2021-06-01 09:41:16 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = http
         } else if (req.url === '/enquetes/sushi-pizza') {
           res.write(pug.renderFile('./form.pug', {
             path: req.url,
-            firstItem: '寿司',
+            firstItem: '寿司',//test
             secondItem: 'ピザ'
           }));
         }
