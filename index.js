625e2d37b5f57a76beac0fe975c0316f1effa50f
BlueZhiaar <tightblackmachine@gmail.com>
2021-10-13 11:16:07 +0900

    日付を削除

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pug = require('pug');
 const server = http
   .createServer((req, res) => {
     const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
+    console.info(' Requested by ' + req.socket.remoteAddress);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -59,7 +59,7 @@ const server = http
             const answer = qs.parse(rawData);
             const body = answer['name'] + 'さんは' +
               answer['favorite'] + 'に投票しました';
-            console.info('[' + now + '] ' + body);
+            console.info(body);
             res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
               body + '</h1></body></html>');
             res.end();
